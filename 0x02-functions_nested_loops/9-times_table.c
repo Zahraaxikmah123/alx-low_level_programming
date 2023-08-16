6c28e059f3c9f5a2e9358d82b6dc32a3db9b9220
zahra <zahraabdulaahi47@gmail.com>
2023-08-16 05:50:26 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,23 +5,35 @@
  */
 void times_table(void)
 {
-int x, y, z, u, product;
+int x, y, z, u;
+
 for (x = 0; x <= 9; x++)
 {
 for (y = 0; y <= 9; y++)
 {
-product = x * y;
+int product = x * y;
 z = product / 10; /* tens digit */
 u = product % 10; /* units digit */
-if (y != 0)
+
+if (y == 0)
+{
+_putchar('0');
+}
+else
 {
 _putchar(',');
 _putchar(' ');
 }
-if (z == 0 && y != 0)
+
+if (z == 0)
+{
 _putchar(' ');
+}
 else
+{
 _putchar(z + '0');
+}
+
 _putchar(u + '0');
 }
 _putchar('\n');
