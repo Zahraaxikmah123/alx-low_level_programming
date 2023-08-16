a18bb80fc9ab4f0f4ae4d03139ed94ca4af889fd
zahra <zahraabdulaahi47@gmail.com>
2023-08-16 05:58:54 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,28 +5,28 @@
  */
 void jack_bauer(void)
 {
-    int a, b, c, d;
+int a, b, c, d;
 
-    for (a = 0; a < 3; a++)
-    {
-        for (b = 0; b < 4; b++)
-        {
-            for (c = 0; c < 6; c++)
-            {
-                for (d = 0; d < 10; d++)
-                {
-                    if (!(a == 2 && b > 3))
-                    {
-                        _putchar(a + '0');
-                        _putchar(b + '0');
-                        _putchar(':');
-                        _putchar(c + '0');
-                        _putchar(d + '0');
-                        _putchar('\n');
-                    }
-                }
-            }
-        }
-    }
+for (a = 0; a < 3; a++)
+{
+for (b = 0; b < 4; b++)
+{
+for (c = 0; c < 6; c++)
+{
+for (d = 0; d < 10; d++)
+{
+if (!(a == 2 && b > 3))
+{
+_putchar(a + '0');
+_putchar(b + '0');
+_putchar(':');
+_putchar(c + '0');
+_putchar(d + '0');
+_putchar('\n');
+}
+}
+}
+}
+}
 }
 
