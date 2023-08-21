8efe4f333a08b0dd34c7f1b87fb1e817be5fc0a0
zahra <zahraabdulaahi47@gmail.com>
2023-08-21 11:16:50 -0700

    7-main.c

--- 0x05-pointers_arrays_strings/7-main.c
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -1,15 +1,33 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * puts_half - Prints the second half of a string, followed by a new line.
+ * @str: The input string.
  */
-int main(void)
+void puts_half(char *str)
 {
-    char *str;
+int length = 0;
+int start;
 
-    str = "0123456789";
-    puts_half(str);
-    return (0);
+while (str[length] != '\0')
+{
+length++;
+}
+
+if (length % 2 == 0)
+{
+start = length / 2;
+}
+else
+{
+start = (length + 1) / 2;
+}
+
+while (str[start] != '\0')
+{
+_putchar(str[start]);
+start++;
+}
+
+_putchar('\n');
 }
---
+++ 0x05-pointers_arrays_strings/main_7.c
@@ -0,0 +1,8 @@
+#include "main.h"
+
+int main(void)
+{
+    char *str = "Lorem ipsum"; /* Your long string here */
+    puts_half(str);
+    return (0);
+}
